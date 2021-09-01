6c8833947936b1c891dc055631a9ee70f3060ee4
MohammedArshad25 <arshadshaik4325@gmail.com>
2021-09-01 23:12:46 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+
+const Navbar = () => {
+    return(
+        <div>
+            Home 
+            About 
+            portfolio
+            blog
+            contact
+        </div>
+    );
+}
+
+export default Navbar;
